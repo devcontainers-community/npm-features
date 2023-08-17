4ac19d34e10b53b7c19a392b7a219223423f20fa
Jacob Hummer <jcbhmr@outlook.com>
2023-08-17 11:00:18 -0500

    proper version

--- generate-feature.sh
+++ generate-feature.sh
@@ -12,7 +12,7 @@ cd src/$1
 cat <<EOF | tee devcontainer-feature.json
 {
   "id": "$1",
-  "version": "1.1.0",
+  "version": "1.0.0",
   "name": "<TODO: Add title>",
   "documentationURL": "http://github.com/devcontainers-community/npm-features/tree/main/src/$1",
   "description": "<TODO: Add description>",
