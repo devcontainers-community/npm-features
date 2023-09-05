6a81705fc64287f2454b4f80f4cd23421faa13df
Jacob Hummer <jcbhmr@outlook.com>
2023-09-05 19:45:59 +0000

    update generation

--- generate-feature.sh
+++ generate-feature.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 set -e
 
-if [[ -e src/$1 || -e test/$1 ]]; then
-  echo "'$1' already exists"
-  exit 1
-fi
+read -p 'Name: ' name
+read -p 'Description: ' description
+read -p 'npm name: ' npm_name
+read -p 'ID: ' id
+read -p 'Test command: ' test_cmd
 
-mkdir src/$1
-pushd src/$1
+mkdir -p src/$id
+cat <<EOF > src/$id/README.md
+# $name Dev Container Feature
 
-cat <<EOF | tee devcontainer-feature.json
+$description
+
+<p align="center">
+  <a href="<TODO: Add link>">
+    <img src="<TODO: Add image>">
+  </a>
+</p>
+
+> <TODO: Add quote>
+
+&mdash; <TODO: Add source>
+
+## Usage
+
+![Codespaces](https://img.shields.io/static/v1?style=for-the-badge&message=Codespaces&color=181717&logo=GitHub&logoColor=FFFFFF&label=)
+![Devcontainers](https://img.shields.io/static/v1?style=for-the-badge&message=Devcontainers&color=2496ED&logo=Docker&logoColor=FFFFFF&label=)
+
+\`\`\`json
+// devcontainer.json
+"features": {
+  "ghcr.io/devcontainers-community/features/$id:1": {}
+}
+\`\`\`
+
+## Options
+
+You can pin a specific version of the npm package like this:
+
+\`\`\`json
+// devcontainer.json
+"features": {
+  "ghcr.io/devcontainers-community/features/$id:1": {
+    "version": "1.2.3"
+  }
+}
+\`\`\`
+EOF
+
+cat <<EOF > src/$id/devcontainer-feature.json
 {
-  "id": "$1",
+  "id": "$id",
   "version": "1.0.0",
-  "name": "<TODO: Add title>",
-  "documentationURL": "http://github.com/devcontainers-community/npm-features/tree/main/src/$1",
-  "description": "<TODO: Add description>",
+  "name": "$name",
+  "documentationURL": "http://github.com/devcontainers-community/npm-features/tree/main/src/$id",
+  "description": "$description",
   "options": {
     "version": {
       "default": "latest",
@@ -30,19 +70,17 @@ cat <<EOF | tee devcontainer-feature.json
 }
 EOF
 
-cat <<EOF | tee install.sh
+cat <<EOF > src/$id/install.sh
 #!/bin/bash
 set -ex
-npm install -g $1@\$VERSION
+npm install -g $npm_name@\$VERSION
 EOF
 
-popd
-
-mkdir test/$1
-pushd test/$1
-
-cat <<EOF | tee test.sh
+if [[ -n $test_cmd ]]; then
+  mkdir -p test/$id
+  cat <<EOF > test/$id/test.sh
 #!/bin/bash
 set -ex
-$1 --version
+$test_cmd
 EOF
+fi
