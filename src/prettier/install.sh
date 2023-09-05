346e490eb423f4196e4bbeec6a3554527e4108a5
Jacob Hummer <jcbhmr@outlook.com>
2023-09-05 15:11:29 -0500

    Update install.sh

--- src/prettier/install.sh
+++ src/prettier/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 npm install -g prettier@$VERSION
-
-# Prettier plugins are expected to be installed locally, not globally
-# In particular, VSCode + extensions tend to have issues with this
 if [[ -n $PLUGINS ]]; then
-  npm install -D $PLUGINS
+  npm install -g $PLUGINS
 fi
